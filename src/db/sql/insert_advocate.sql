b05744d5057926ddf42ae6c936088bd926b2a169
Daniel Kimball <danielyoungkimball@outlook.com>
2025-07-12 18:15:43 -0700

    Integrate with DB

---
+++ src/db/sql/insert_advocate.sql
@@ -0,0 +1,18 @@
+INSERT INTO advocates (
+        first_name,
+        last_name,
+        city,
+        degree,
+        payload,
+        years_of_experience,
+        phone_number
+    )
+VALUES (
+        'Daniel',
+        'Kimball',
+        'Irvine',
+        'BCS',
+        '["Sleep issues", "Personal growth"]',
+        5,
+        4156948655
+    );
